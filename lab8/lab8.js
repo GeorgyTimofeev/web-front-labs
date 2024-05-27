f7f5916bf2aec0071b137f9dabb65b91f3b1382d
GeorgyTimofeev <timofeevga22@gmail.com>
2024-05-28 01:05:09 +0700

    Вывод даты

---
+++ lab8/lab8.js
@@ -0,0 +1,44 @@
+function showDate() {
+  let out = document.getElementById("current-date");
+  let today = new Date();
+  out.innerHTML = "Русская локаль: " + today.toLocaleString("ru-RU");
+
+  let out1 = document.getElementById("current-date1");
+  out1.innerHTML = "Китайская локаль: " + today.toLocaleString("zh-CN");
+
+  let out2 = document.getElementById("current-date2");
+  out2.innerHTML = "Арабская локаль: " + today.toLocaleString("ar");
+
+  let out3 = document.getElementById("current-date3");
+  out3.innerHTML = "Тайская локаль: " + today.toLocaleString("th");
+
+  let out4 = document.getElementById("current-date4");
+  out4.innerHTML = "Египетская локаль: " + today.toLocaleString("ar-EG");
+
+  let year = today.getFullYear();
+  let mounth = today.getMonth();
+  let date = today.getDate();
+  let day = today.getDay();
+
+  let mounthArr = [
+    "Январь",
+    "Февраль",
+    "Март",
+    "Апрель",
+    "Май",
+    "Июнь",
+    "Июль",
+    "Август",
+    "Сентябрь",
+    "Октябрь",
+    "Ноябрь",
+    "Декабрь",
+  ];
+
+  document.getElementById("year").innerHTML = "Сегодняшний год: " + year;
+  document.getElementById("mounth").innerHTML =
+    "Сегодняшний месяц: " + mounthArr[mounth];
+  document.getElementById("day").innerHTML = "Сегодняшний день: " + date;
+  document.getElementById("dayWeek").innerHTML =
+    "Сегодняшний день недели: " + dayArr[day];
+}
